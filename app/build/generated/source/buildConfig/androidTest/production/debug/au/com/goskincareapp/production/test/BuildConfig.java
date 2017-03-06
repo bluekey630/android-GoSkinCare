99acd498dc256d80a8ee4a71023e6a03eced5882
Bluekey <zhengcheng129@outlook.com>
2017-03-06 11:42:42 +0800

    initialization
    6/3/2017

---
+++ app/build/generated/source/buildConfig/androidTest/production/debug/au/com/goskincareapp/production/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package au.com.goskincareapp.production.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "au.com.goskincareapp.production.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "production";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "1.01-production";
+}
